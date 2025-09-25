c1fee38bf171d8c50078f5ec575ddfd1a9c75b3f
Shahbaz Youssefi <syoussefi@google.com>
2025-09-25 12:50:48 -0700

    Revert "Disable VK_EXT_graphics_pipeline_library until debugged"
    This reverts commit 3cffff6267ed5df4f9e95b3bace358652945bb45.
    
    Reason for revert: Underlying bug is fixed
    
    Bug: b/439531864
    Change-Id: I7f9756dc9db26819bcb20bc416402ea8b1fbc385

--- include/config/SkUserConfigManual.h
+++ include/config/SkUserConfigManual.h
@@ -21,10 +21,6 @@
   // Check error is expensive. HWUI historically also doesn't check its allocations
   #define GR_GL_CHECK_ALLOC_WITH_GET_ERROR 0
 
-  // Disable these Vulkan features until debugged
-  // b/439531864
-  #define SK_DISABLE_GRAPHICS_PIPELINE_LIBRARY
-
   // Graphite features
   #define SK_TRACE_GRAPHITE_PIPELINE_USE
 
